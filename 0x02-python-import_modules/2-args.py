a65c0ba1c781b29de6d9a4e5c09024ed1ea3576d
AlpagaSauvage <3645@holbertonschool.com>
2021-12-23 13:49:32 +0000

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("0 arguments")
+    else:
+        length = len(sys.argv)
+        if length == 2:
+            print("{} argument:".format(length - 1))
+        else:
+            print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
