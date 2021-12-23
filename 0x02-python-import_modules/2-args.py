e3a5d74130e11a2fd2542b091e35d1ba30668328
AlpagaSauvage <3645@holbertonschool.com>
2021-12-23 13:51:47 +0000

    modified task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print("0 arguments")
+        print("0 arguments.")
     else:
         length = len(sys.argv)
         if length == 2:
