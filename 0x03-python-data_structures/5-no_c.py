bff5c156a45faedd1e61e596166db9e85f8cf17d
AlpagaSauvage <3645@holbertonschool.com>
2022-01-03 16:42:36 +0000

    add task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    characters = "cC"
-    for x in range(len(characters)):
-        my_string = my_string.replace(characters[x], "")
-    return my_string
+    return(my_string.translate({ord('c'): None, ord('C'): None}))
