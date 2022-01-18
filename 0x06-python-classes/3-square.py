3a02c15de70d82f3d853ab3a89fe411d8a3669a4
AlpagaSauvage <3645@holbertonschool.com>
2022-01-18 17:14:29 +0000

    add task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Classe Square
+    print square with varianble size
+"""
+
+
+class Square:
+    """
+    define size square
+
+    Attibutes
+    ---------
+    size : int
+        the size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int
+            the size of the square.
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
