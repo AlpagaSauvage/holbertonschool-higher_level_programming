cadb0320cb0d62277799136ea9e104f84aea5fc2
AlpagaSauvage <3645@holbertonschool.com>
2022-01-25 18:24:02 +0000

    add all task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        return int(a + b)
+    except:
+        if type(a) is not int:
+            raise TypeError("a must be an integer")
+        if type(b) is not int:
+             raise TypeError("b must be an integer")
