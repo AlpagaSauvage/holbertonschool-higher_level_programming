75ee95462a8b567fc2f0c50377170756a56bce82
AlpagaSauvage <3645@holbertonschool.com>
2022-01-27 17:20:38 +0000

    add betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create an hash tables
+ * @size: size of the hash
+ * Return: new hash
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
