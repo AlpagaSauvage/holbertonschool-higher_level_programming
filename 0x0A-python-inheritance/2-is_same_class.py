15049c23f9f42e65b8f5ab40faacf1f642ce27b3
AlpagaSauvage <3645@holbertonschool.com>
2022-01-31 17:05:25 +0000

    add docu

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" 2-is_same_class
+    same class
+"""
+
+
 def is_same_class(obj, a_class):
+    """is same class"""
     return type(obj) is a_class
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" 3-is_king_of_class
+    king of class
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """is king of class"""
     if isinstance(obj, a_class):
         return True
     else:
