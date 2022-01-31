8c24cbc89101db6faef650f85e0a867568371969
AlpagaSauvage <3645@holbertonschool.com>
2022-01-31 20:48:46 +0000

    add task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""100-my_int.py:
+    my int
+"""
+
+
+class MyInt(int):
+    """MyInt"""
+    def __eq__(self, other):
+        return int.__index__ == other
+
+    def __ne__(self, other):
+        return int.__index != other
