e56afd85f861526d3549361630dd90ab6cb2fc88
AlpagaSauvage <3645@holbertonschool.com>
2022-01-31 20:55:20 +0000

    add task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 101-add_attribute
+    add attribute
+"""
+
+
+def add_attribute(obj, name, value):
+    """add attribute"""
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
