5b0c10a92fd355d4f6242f7586e6a112dbd6632b
AlpagaSauvage <3645@holbertonschool.com>
2022-01-31 16:56:44 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""6-base_geometry.py:
+    Base geometry
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
