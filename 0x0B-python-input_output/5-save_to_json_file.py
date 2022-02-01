61534439b21d10899d038fa585905de405310ebe
AlpagaSauvage <3645@holbertonschool.com>
2022-02-01 18:01:36 +0000

    add task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" 5-save_to_json_file
+    save object to a file
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save object to a file"""
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
