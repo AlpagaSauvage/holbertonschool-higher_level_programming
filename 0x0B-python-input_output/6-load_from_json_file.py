c930510cdd644cd4eb86fc157152093a88442dbc
AlpagaSauvage <3645@holbertonschool.com>
2022-02-01 18:13:33 +0000

    add task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 6-load_from_json_file
+    return from json file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, 'r') as f:
+        f.read()
