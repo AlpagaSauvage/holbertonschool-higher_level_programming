b96460dc81f60aa420684a6f85341607fb1aebb8
AlpagaSauvage <3645@holbertonschool.com>
2022-02-07 13:53:18 +0000

    add square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""square.py:
+    print a square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """classe Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """init of square"""
+        self.__size = size
+        self.__x = x
+        self.__y = y
+        self.id = id
+        super().__init__(id, x, y, size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def __str__(self):
+        """ Definition of the square """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}"\
+            .format(self.id, self.__x, self.__y, self.__size)
