d2dc7c3df2d070be8884f06712540710a08184b6
AlpagaSauvage <3645@holbertonschool.com>
2022-02-07 17:51:15 +0000

    modified  task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """to json string"""
         if list_dictionaries is None or list_dictionaries is Empty:
-            return "[]"
+            return []
         else:
             return json.dumps(list_dictionaries)
