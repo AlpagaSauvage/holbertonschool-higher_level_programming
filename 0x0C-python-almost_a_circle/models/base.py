ba1fb7875c14a3949744dc07139a562a28031981
AlpagaSauvage <3645@holbertonschool.com>
2022-02-07 17:48:21 +0000

    add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,13 +4,24 @@
 """
 
 
+import json
+
+
 class Base:
     """Class Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """init"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """to json string"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
