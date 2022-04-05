a665a972ee27c99b5a0b698ea89658b35aca15fc
AlpagaSauvage <3645@holbertonschool.com>
2022-04-05 12:48:51 +0000

    add task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let i = 0;
-	let count = 0;
-	while (list[i] !== undefined) {
-		if(list[i] === searchElement) {
-			count++;
-		}
-		i++;
-	}
-	return (count);
-}
+  let i = 0;
+  let count = 0;
+  while (list[i] !== undefined) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+    i++;
+  }
+  return (count);
+};
