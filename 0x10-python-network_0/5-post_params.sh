d952f423235ccd1aee8f3d587146bd56e0843c12
AlpagaSauvage <3645@holbertonschool.com>
2022-04-21 20:19:34 +0000

    modified task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL POST parameters
-curl -sv 'email=test@gmail.com' -v 'subject=I will always be here for PLD' -X POST "$1"
+curl -sv 'email=test@gmail.com' 'subject=I will always be here for PLD' -X POST "$1"
