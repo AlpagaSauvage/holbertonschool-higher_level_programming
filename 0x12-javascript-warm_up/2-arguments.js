79f0d36c37df76719720d5bdc14e38fc96c2aec0
AlpagaSauvage <90853574+AlpagaSauvage@users.noreply.github.com>
2022-07-22 15:16:40 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else if (process.argv[3] === undefined && process.argv[2]) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let c = 0;
-while (myVar[c] !== undefined) {
-  console.log(myVar[c]);
-  c++;
-}
