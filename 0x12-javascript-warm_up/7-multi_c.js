9fb17c25bb427a37af3e146c698fe80e015782e5
AlpagaSauvage <3645@holbertonschool.com>
2022-08-01 10:20:33 +0000

    repare task

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const myVar = process.argv[2];
 if (parseInt(myVar)) {
   for (let x = 0; x < myVar; ++x) {
-    console.log('C in fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
