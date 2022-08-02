8fc4988ba2e4ab3fbdedb652bed7b07ed1a9bd71
AlpagaSauvage <3645@holbertonschool.com>
2022-08-02 11:19:27 +0000

    add task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,30 +1,7 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./4-rectangle');
 
-  print () {
-    for (let x = 0; x < this.height; ++x) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    this.temp = this.width;
-    this.width = this.height;
-    this.height = this.temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
 
 class Square extends Rectangle {
   constructor (size) {
