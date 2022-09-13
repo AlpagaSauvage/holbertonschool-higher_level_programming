77f4b4cd1de079f3c74f27d481eda5612f396702
AlpagaSauvage <3645@holbertonschool.com>
2022-09-13 15:51:14 +0000

    add task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,11 +3,11 @@
 const axios = require('axios');
 
 axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2]).then(resp => {
-	for (const char of resp.data.characters) {
-		axios.get(char)
-		  .then(res => {
-			console.log(res.data.name);
-		  })
-	}
+  for (const char of resp.data.characters) {
+    axios.get(char)
+      .then(res => {
+        console.log(res.data.name);
+      });
+  }
 })
-.catch(err => console.log(err));
+  .catch(err => console.log(err));
