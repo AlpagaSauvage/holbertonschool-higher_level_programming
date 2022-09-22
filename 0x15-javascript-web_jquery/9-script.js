fdef7c44bdaac056535ba534a09c3da7b28e5545
AlpagaSauvage <3645@holbertonschool.com>
2022-09-22 13:27:24 +0000

    modified task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,3 @@
-$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-  $('DIV#hello').append(data.hello);
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', window.onload = function (data) {
+  $('DIV#hello').text(data.hello);
 });
